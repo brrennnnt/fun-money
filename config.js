9925e853644fb6acbbb8f9550d5fc71de8b0c760
brrennnnt <brentroth15@gmail.com>
2026-08-27 12:04:16 -0700

    config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAVFOllU9pz3XN1CJ3iaaoaBmXxO_aEQ6M",
+  authDomain: "fun-money-f4820.firebaseapp.com",
+  projectId: "fun-money-f4820",
+  storageBucket: "fun-money-f4820-firebasestorage.app",
+  messagingSenderId: "507596731983",
+  appId: "1:507596731983:web:f9713394294088ae865c3a"
+};
